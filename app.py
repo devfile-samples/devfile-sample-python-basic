1b7fac78436e09bab63d03bbc79002dc5a258a4b
eyuen <eyuen@redhat.com>
2021-04-30 14:08:05 -0400

    Add waitress and resolve port conflict
    Signed-off-by: eyuen <eyuen@redhat.com>

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    serve(app, host='0.0.0.0', port=8080)
