f70c2937d5303704fb423d83478ac94efdf96f8d
Paul Schultz <pschultz@pobox.com>
2022-05-26 13:15:54 -0500

    replaced if statement with ternary
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('FLASK_PORT'))
-
-    if port == None:
-        port = 8080
+    port = int(os.environ.get('FLASK_PORT')) or 8080
 
     app.run(port=port,host='0.0.0.0')
