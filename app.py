e6c551e88cb30e84cf5b414b2a5ea489bddfec03
eaespinosa <65977866+eaespinosa@users.noreply.github.com>
2023-06-26 00:59:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to the Flask app by Edgar A. Espinosa!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
