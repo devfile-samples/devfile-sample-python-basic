a5dc38c7f487ab317b74b7ebf140dc429038adc6
Hongwei Liu <34546888+hongweiliu17@users.noreply.github.com>
2024-02-27 22:45:07 +0800

    Revert "test"

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Red Hat!"
+    return "Hello World!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
