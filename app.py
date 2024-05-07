4ab95e5d6c16146448f18aa6cfde46b5ff20c766
Marcel Hild <hild@b4mad.net>
2024-05-07 21:10:22 +0200

    Update hello route to return "Hallo Welt!!!" instead of "Hallo Welt!"
    Signed-off-by: Marcel Hild <hild@b4mad.net>

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hallo Welt!"
+    return "Hallo Welt!!!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
