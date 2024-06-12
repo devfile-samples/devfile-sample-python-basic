01cc4ddd9adfa1660e12e2a28805e2c4ce41c587
Ales Raszka <araszka@redhat.com>
2024-06-12 14:40:42 +0200

    new change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World 2!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
