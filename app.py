fd3777b4ff98351cb2feff189aca3aa218a25e4d
Joel Minin Dsouza <joel.dsouza@mindcraft.in>
2024-11-11 14:46:44 +0530

    Date

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from platform import python_version
 import os
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     version = "\nCurrent Version of Python interpreter - "+ python_version()
+    date_var = datetime.datetime.now()
+    output_str = "Hello World!<br><br>Current Date:"+str(date_var)+"<br><br>"+version
+    print("Current Date: "+date_var)
     print("Current Version of Python interpreter -", python_version())
-    return version
+    return output_str
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
