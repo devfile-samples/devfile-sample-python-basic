332345a4a7460f2449e432aec0f4001c2ca369e4
Joel Minin Dsouza <joel.dsouza@mindcraft.in>
2024-11-11 14:55:58 +0530

    Pushing new Chnages!!

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    version = "\nCurrent Version of Python interpreter - "+ python_version()
+    version = "\nCurrent Version of Python interpreter : "+ python_version()
     date_var = datetime.datetime.now()
-    output_str = "Hello World!<br><br>Current Date:"+str(date_var)+"<br><br>"+version
-    print("Current Date: "+str(date_var))
-    print("Current Version of Python interpreter -", python_version())
+    output_str = "Hello World!<br><br>Current Date : "+str(date_var)+"<br><br>"+version
+    print("Current Date : "+str(date_var))
+    print("Current Version of Python interpreter : ", python_version())
     return output_str
 
 if __name__ == '__main__':
