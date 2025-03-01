9c6e16bc330060c69f48a55e5672acad523a2655
adrielmendezrios <adriel315@gmail.com>
2025-03-01 11:47:27 -0500

    added about

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return "my names jeff"
 
+@app.route('/about')
+def about():
+    return jsonify({"how":"through openshift"})
+
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
